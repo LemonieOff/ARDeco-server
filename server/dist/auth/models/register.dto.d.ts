e8d4e84ca63bbba676530ee373f3685cd5281198
IndiGan <72012215+IndiGan@users.noreply.github.com>
2023-04-16 15:12:57 +0200

    Remove dist and node_modules folders from te repo

--- server/dist/auth/models/register.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class RegisterDto {
-    email: string;
-    password: string;
-    password_confirm: string;
-    first_name: string;
-    city: string;
-    last_name: string;
-    phone: string;
-}
--- server/dist/user/models/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    id: number;
-    email: string;
-    first_name: string;
-    last_name: string;
-    phone: string;
-    city: string;
-    password: string;
-}
