a54a12313b45c5436849552234e19652331a1a4b
IndiGan <72012215+IndiGan@users.noreply.github.com>
2023-05-13 17:32:14 +0200

    Add catalog entity

---
+++ server/src/catalog/models/catalog.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('catalog')
+export class Catalog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  styles: string; // JSON array ["style1", "style2", "style3"]
+
+  @Column()
+  rooms: string; // JSON array ["room1", "room2", "room3"]
+
+  @Column()
+  width: number;
+
+  @Column()
+  height: number;
+
+  @Column()
+  depth: number;
+
+  @Column()
+  colors: string; // JSON array ["color1", "color2", "color3"]
+
+  @Column()
+  number: number; // Partner ID
+}
