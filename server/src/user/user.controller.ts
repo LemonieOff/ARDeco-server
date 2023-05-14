8dda9768eb01f2bb811e4ff11a5490e824229941
IndiGan <72012215+IndiGan@users.noreply.github.com>
2023-05-14 14:04:50 +0200

    Better user edit

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -27,20 +27,33 @@ export class UserController {
 
     @UseGuards(AuthGuard)
     @Put()
-    editViaQuery(@Req() req: Request, @Body() user: User) {
-        const cookie = req.cookies['jwt']
-        const data = this.jwtService.verify(cookie)
-        return this.userService.update(data['id'], user)
+    async editViaQuery(@Req() req: Request, @Body() user: User, @Res({passthrough: true}) res: Response) {
+        const id = Number(req.query['id']);
+        if (id === undefined || isNaN(id)) {
+            res.status(400);
+            return {
+                "status": "KO",
+                "code": 400,
+                "description": "User was not updated because of an error",
+                "error": "User ID must be passed as a query parameter and be a number",
+                "data": null
+            }
+        }
+        return await this.editUser(req, Number(req.query["id"]), user, res)
     }
 
     @UseGuards(AuthGuard)
     @Put(":id")
     async editViaParam(@Req() req: Request, @Param("id") id: number, @Body() user: User, @Res({passthrough: true}) res: Response) {
-        const cookie = req.cookies['jwt']
-        const data = this.jwtService.verify(cookie)
-        const request_user_id = await this.userService.findOne({id: data['id']})
+        return await this.editUser(req, id, user, res)
+    }
+
+    async editUser(req: Request, id: number, user: User, res: Response) {
+        const cookie = req.cookies['jwt'];
+        const data = this.jwtService.verify(cookie);
+        const request_user_id = await this.userService.findOne({id: data['id']});
         if (data['id'] != id && request_user_id['role'] != 'admin') {
-            res.status(401)
+            res.status(401);
             return {
                 "status": "KO",
                 "code": 401,
@@ -49,7 +62,7 @@ export class UserController {
             }
         }
         if (user.role !== undefined && request_user_id['role'] != 'admin') {
-            res.status(401)
+            res.status(401);
             return {
                 "status": "KO",
                 "code": 401,
