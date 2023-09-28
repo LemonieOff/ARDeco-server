7e5d9409825f5d2d5b1dccc3def0b8261680035d
IndiGan <72012215+IndiGan@users.noreply.github.com>
2023-09-28 14:37:30 +0200

    Update .prettierrc for internal coding style

--- server/src/order_history/models/order_history.entity.ts
+++ server/src/order_history/models/order_history.entity.ts
@@ -1,20 +1,19 @@
-/* eslint-disable prettier/prettier */
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity('order_history')
+@Entity("order_history")
 export class OrderHistory {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({type: "int", update: false})
+    @Column({ type: "int", update: false })
     user_id: number;
 
-    @Column({type: "timestamp", default: () => "CURRENT_TIMESTAMP", update: false})
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", update: false })
     datetime: Date;
 
-    @Column({type: "float"})
+    @Column({ type: "float" })
     total_amount: number;
 
-    @Column({type: "json"})
+    @Column({ type: "json" })
     furniture: string;
 }
