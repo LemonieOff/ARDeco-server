f087cf6fcdaaa2e7732f1c06270fdda0e9d19e83
louis <louis.villedieu@epitech.eu>
2024-01-17 20:15:25 -0500

    add debug logs

--- server/src/ticket/ticket.controller.ts
+++ server/src/ticket/ticket.controller.ts
@@ -31,8 +31,10 @@ export class TicketController {
   //  @UseGuards(AuthGuard)
     @Get('pending')
     async getPending(@Req() req: Request): Promise<any> {
+        console.log(req.cookies);
         console.log("jwt in cookie is " + req.cookies['jwt']);
         const data = await this.jwtService.verifyAsync(req.cookies['jwt'])
+        console.log(data);
         const usr = await this.userService.findOne({id: data['id']})
         console.log("User", usr);
         if (usr.role != "admin") {
@@ -100,8 +102,10 @@ export class TicketController {
    // @UseGuards(AuthGuard)
     @Get('stats')
     async getStats(@Req() req: Request): Promise<any> {
+        console.log(req.cookies);
         console.log("jwt in cookie is " + req.cookies['jwt']);
         const data = await this.jwtService.verifyAsync(req.cookies['jwt'])
+        console.log(data);
         const usr = await this.userService.findOne({id: data['id']})
         console.log("User", usr);
         if (usr.role != "admin") {
@@ -143,8 +147,10 @@ export class TicketController {
     //@UseGuards(AuthGuard)
     @Get('stats/last7days')
     async getStatsLast7Days(@Req() req: Request): Promise<any> {
+        console.log(req.cookies);
         console.log("jwt in cookie is " + req.cookies['jwt']);
         const data = await this.jwtService.verifyAsync(req.cookies['jwt'])
+        console.log(data);
         const usr = await this.userService.findOne({id: data['id']})
         console.log("User", usr);
         console.log("Role", usr.role);
