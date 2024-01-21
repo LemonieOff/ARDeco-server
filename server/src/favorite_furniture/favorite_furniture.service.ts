9244c9edc5b72dca2c3d212e77a419920c5f3a31
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-01-21 22:28:20 +0100

    Main methods for FavoriteFurnitureService

--- server/src/favorite_furniture/favorite_furniture.service.ts
+++ server/src/favorite_furniture/favorite_furniture.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import {  FindManyOptions, FindOptionsWhere, Repository  } from "typeorm";
+import { FindManyOptions, FindOptionsWhere, Repository } from "typeorm";
 import { FavoriteFurniture } from "./models/favorite_furniture.entity";
 
 @Injectable()
@@ -10,62 +10,44 @@ export class FavoriteFurnitureService {
         private readonly favoriteFurnitureRepository: Repository<FavoriteFurniture>
     ) {}
 
-    // async create(data): Promise<FavoriteFurniture> {
-    //     try {
-    //         JSON.parse(data.furniture);
-    //     } catch (e) {
-    //         return await new Promise((_, reject) => {
-    //             reject({
-    //                 error: "JsonError",
-    //                 message: "Furniture is not a valid JSON object",
-    //                 furniture: data.furniture
-    //             });
-    //         });
-    //     }
-    //     const item = await this.favoriteFurnitureRepository.save(data);
-    //     console.log("Create favorite_furniture item :", item);
-    //     return item;
-    // }
-    
-    // async findAll(
-    //     user_id: number | null,
-    //     limit: number | null,
-    //     begin_pos: number | null
-    // ): Promise<FavoriteFurniture[]> {
-    //     let where: FindOptionsWhere<FavoriteFurniture> = { /*visibility: true*/ }; // Public items only
-    //     if (user_id) {
-    //         where = {
-    //             ...where,
-    //             user_id: user_id
-    //         };
-    //     }
+    async create(data): Promise<FavoriteFurniture> {
+        const item = await this.favoriteFurnitureRepository.save(data);
+        console.log("Create favorite_furniture item :", item);
+        return item;
+    }
 
-    //     let options: FindManyOptions<FavoriteFurniture> = { where: where };
-    //     if (limit) {
-    //         options = {
-    //             ...options,
-    //             take: limit
-    //         };
-    //     }
-    //     if (begin_pos && limit) {
-    //         options = {
-    //             ...options,
-    //             skip: begin_pos
-    //         };
-    //     }
-    //     return this.favoriteFurnitureRepository.find(options);
-    // }
-    // async findOne(where: FindOptionsWhere<FavoriteFurniture>): Promise<FavoriteFurniture> {
-    //     return this.favoriteFurnitureRepository.findOne({ where: where });
-    // }
+    async findAll(
+        user_id: number | null = null,
+        limit: number | null = null,
+        begin_pos: number | null = null
+    ): Promise<FavoriteFurniture[]> {
+        let where: FindOptionsWhere<FavoriteFurniture> = {};
+        if (user_id) {
+            where = {
+                ...where,
+                user_id: user_id
+            };
+        }
 
+        let options: FindManyOptions<FavoriteFurniture> = { where: where };
+        if (limit) {
+            options = {
+                ...options,
+                take: limit
+            };
+        }
+        if (begin_pos && limit) {
+            options = {
+                ...options,
+                skip: begin_pos
+            };
+        }
+        return this.favoriteFurnitureRepository.find(options);
+    }
 
-    // async delete(id: number): Promise<any> {
-    //     return this.favoriteFurnitureRepository
-    //         .createQueryBuilder("gallery")
-    //         .delete()
-    //         .from(FavoriteFurniture)
-    //         .where("id = id", { id: id })
-    //         .execute();
-    // } 
+    async findOne(
+        where: FindOptionsWhere<FavoriteFurniture>
+    ): Promise<FavoriteFurniture> {
+        return this.favoriteFurnitureRepository.findOne({ where: where });
+    }
 }
