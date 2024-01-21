929fe0359f4055222f17ad7539f4491f0b3e2633
LoïcP <loic.planchon@epitech.eu>
2024-01-21 10:42:30 -0800

    Fav Gallery/furniture request Post/get/del not completely working

--- server/src/favorite_gallery/favorite_gallery.service.ts
+++ server/src/favorite_gallery/favorite_gallery.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import {  FindManyOptions, FindOptionsWhere, Repository  } from "typeorm";
 import { FavoriteGallery } from "./models/favorite_gallery.entity";
 
 @Injectable()
@@ -25,5 +25,48 @@ export class FavoriteGalleryService {
         const item = await this.favoriteGalleryRepository.save(data);
         console.log("Create favorite_gallery item :", item);
         return item;
+    }
+
+
+    async findAll(
+        user_id: number | null,
+        limit: number | null,
+        begin_pos: number | null
+    ): Promise<FavoriteGallery[]> {
+        let where: FindOptionsWhere<FavoriteGallery> = { /*visibility: true*/ }; // Public items only
+        if (user_id) {
+            where = {
+                ...where,
+                user_id: user_id
+            };
+        }
+
+        let options: FindManyOptions<FavoriteGallery> = { where: where };
+        if (limit) {
+            options = {
+                ...options,
+                take: limit
+            };
+        }
+        if (begin_pos && limit) {
+            options = {
+                ...options,
+                skip: begin_pos
+            };
+        }
+        return this.favoriteGalleryRepository.find(options);
+    }
+    async findOne(where: FindOptionsWhere<FavoriteGallery>): Promise<FavoriteGallery> {
+        return this.favoriteGalleryRepository.findOne({ where: where });
+    }
+
+
+    async delete(id: number): Promise<any> {
+        return this.favoriteGalleryRepository
+            .createQueryBuilder("gallery")
+            .delete()
+            .from(FavoriteGallery)
+            .where("id = id", { id: id }) // chamger id par furniture Id
+            .execute();
     }
 }
