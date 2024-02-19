480ad36acefc82ffa99b15221fc31ecad29cd6ef
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-02-19 05:27:42 +0100

    Add new settings

--- server/src/user_settings/models/user_settings.entity.ts
+++ server/src/user_settings/models/user_settings.entity.ts
@@ -17,4 +17,16 @@ export class UserSettings {
 
     @Column({ type: "boolean", default: true })
     sounds_enabled: boolean;
+
+    @Column({ type: "boolean", default: false })
+    dark_mode: boolean;
+
+    @Column({ type: "boolean", default: false })
+    automatic_new_gallery_share: boolean;
+
+    @Column({ type: "boolean", default: false })
+    display_surname_on_public: boolean;
+
+    @Column({ type: "boolean", default: false })
+    display_email_on_public: boolean;
 }
