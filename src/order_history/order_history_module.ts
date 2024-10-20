d02be3cfa88bfd4e357db152857f55c4460a4dd1
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-10-20 23:34:57 +0200

    Add cart dependency to order history module

--- src/order_history/order_history_module.ts
+++ src/order_history/order_history_module.ts
@@ -5,6 +5,7 @@ import { OrderHistory } from "./models/order_history.entity";
 import { OrderHistoryController } from "./order_history_controller";
 import { OrderHistoryService } from "./order_history_service";
 import { UserModule } from "../user/user.module";
+import { CartService } from "../cart/cart.service";
 
 @Module({
     imports: [
@@ -12,10 +13,12 @@ import { UserModule } from "../user/user.module";
         TypeOrmModule.forFeature([OrderHistory]),
         JwtModule.register({
             secret: "secret"
-        })
+        }),
+        CartService
     ],
     controllers: [OrderHistoryController],
     providers: [OrderHistoryService],
     exports: [OrderHistoryService]
 })
-export class OrderHistoryModule {}
+export class OrderHistoryModule {
+}
