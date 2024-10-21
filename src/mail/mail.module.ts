42a84fc1e0a8c37f0e0ce1f4c798b428ab973e85
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-10-21 02:16:17 +0200

    Remove payment systems

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,16 +1,10 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { MailService } from "./mail.service";
-import { join } from "path";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { PaymentsModule } from "src/payments/payments.module";
-import { PaymentsService } from "src/payments/payments.service";
 
 @Module({
-    imports: [
-        forwardRef(() => PaymentsModule),
-        ConfigModule
-    ],
+    imports: [],
     providers: [MailService],
     exports: [MailService]
 })
-export class MailModule {}
+export class MailModule {
+}
